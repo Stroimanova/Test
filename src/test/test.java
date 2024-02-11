510bfa35a387debcb4fa7145730420aecaafa789
unknown <simonastroimanova@hotmail.com>
2024-02-11 21:25:09 +0100

    add first java program

---
+++ src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test {
+	public static void main(String[] args){  
+		//creating instance of the encapsulated class  
+		student s=new student();  
+		//setting value in the name member  
+		s.setName("vijay");  
+		//getting value of the name member  
+		System.out.println(s.getName());  
+		}  
+}
